73631a495ddd8d753d9bcc80c12b6227cef0f478
JFHarris <JFHarris1983@gmail.com>
2022-11-19 18:34:51 -0600

    Hiding Snake on small screens

--- src/components/Weather/TopButtons.js
+++ src/components/Weather/TopButtons.js
@@ -24,7 +24,7 @@ function TopButtons({ setQuery }) {
     }
   ]
   return (
-    <div className="flex items-center justify-around my-6">
+    <div className="flex items-center justify-between my-6">
       {cities.map((city) =>(
         <button
         key={city.id}
